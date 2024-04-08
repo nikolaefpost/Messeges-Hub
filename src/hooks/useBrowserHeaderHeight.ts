a6a4a4ca1b7214c772897ee528a2702db6a65e5f
Yemelyanov <nikolaef.post@gmail.com>
2024-04-08 12:28:34 +0300

    feat: fix mobil browser header

--- src/hooks/useBrowserHeaderHeight.ts
+++ src/hooks/useBrowserHeaderHeight.ts
@@ -4,23 +4,8 @@ export const useBrowserHeaderHeight = () =>  {
     const [browserHeaderHeight, setBrowserHeaderHeight] = useState(0);
 
     useEffect(() => {
-        const handleScroll = () => {
-            const currentScrollPosition = window.scrollY;
-            const scrollDirection = currentScrollPosition > previousScrollPosition ? 'down' : 'up';
-            if (scrollDirection === 'down') {
-                setBrowserHeaderHeight(0); // Assuming the browser header is hidden
-            } else {
-                setBrowserHeaderHeight(window.outerHeight - window.innerHeight);
-            }
-            previousScrollPosition = currentScrollPosition;
-        };
-
-        let previousScrollPosition = window.scrollY;
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
+        console.log(window.outerHeight + "IN" + window.innerHeight)
+        setBrowserHeaderHeight(window.outerHeight - window.innerHeight);
     }, []);
 
     return browserHeaderHeight
